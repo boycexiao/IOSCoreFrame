d804a2fef135dc00b6955282f8d4c5cedeb0b56e
XiaoBin <xiaobin@admintekiMac-mini.local>
2013-08-02 09:18:52 +0800

    Initial Commit

---
+++ IOSCoreFrame/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  IOSCoreFrame
+//
+//  Created by XiaoBin on 13-8-2.
+//  Copyright (c) 2013年 XiaoBin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
